9660334b819616a9252e0b6e25a85796f3a40d22
Korwit <korawit.r@ku.th>
2019-12-01 00:36:20 +0700

    add more pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const DropDownCenterBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 25vw;
+`;
+
+const IndividualDropBoxAlignment = styled.div`
+  margin: 5vh;
+`;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    Page1
-  </Layout>
+  <Container>
+    <Layout>
+      <DropDownCenterBox>
+        <IndividualDropBoxAlignment>
+          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item>Action</Dropdown.Item>
+            <Dropdown.Item>Another action</Dropdown.Item>
+            <Dropdown.Item>Something else</Dropdown.Item>
+          </DropdownButton>
+        </IndividualDropBoxAlignment>
+        <IndividualDropBoxAlignment>
+          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item>Action</Dropdown.Item>
+            <Dropdown.Item>Another action</Dropdown.Item>
+            <Dropdown.Item>Something else</Dropdown.Item>
+          </DropdownButton>
+        </IndividualDropBoxAlignment>
+        <IndividualDropBoxAlignment>
+          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item>Action</Dropdown.Item>
+            <Dropdown.Item>Another action</Dropdown.Item>
+            <Dropdown.Item>Something else</Dropdown.Item>
+          </DropdownButton>
+        </IndividualDropBoxAlignment>
+      </DropDownCenterBox>
+      <SEO title="Temp" />
+      Page1
+    </Layout>
+  </Container>
 );
 
 export default IndexPage;
--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -4,11 +4,11 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const SecondPage = () => (
+const ThirdPage = () => (
   <Layout>
     <SEO title="Page two" />
     Page3
   </Layout>
 );
 
-export default SecondPage;
+export default ThirdPage;
