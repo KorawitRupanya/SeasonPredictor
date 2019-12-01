10bc1c8f987b6c352bcf0389c76980a3e3853c69
Gunthee <sirarat.t@ku.th>
2019-12-02 05:27:06 +0700

    added forecast temp

--- src/components/Predictor.js
+++ src/components/Predictor.js
@@ -34,15 +34,54 @@ const Predictor = props => {
   const [summer, setSummer] = useState('');
   const [rainy, setRainy] = useState('');
   const [winter, setWinter] = useState('');
+  const [summerFc, setSummerFc] = useState('');
+  const [rainyFc, setRainyFc] = useState('');
+  const [winterFc, setWinterFc] = useState('');
   const province = props.province;
   const year = props.year;
+
+  async function fetchDataSummerFc() {
+    const res = await fetch(
+      `http://localhost:3000/${province}/summer/${year - 1}`,
+    );
+    res
+      .json()
+      .then(res => {
+        setSummerFc(res[0].date + ' ' + res[0].month);
+      })
+      .catch(err => setErrors(err));
+  }
+
+  async function fetchDataRainyFc() {
+    const res = await fetch(
+      `http://localhost:3000/${province}/rainy/${year - 1}`,
+    );
+    res
+      .json()
+      .then(res => {
+        setRainyFc(res[0].date + ' ' + res[0].month);
+      })
+      .catch(err => setErrors(err));
+  }
+
+  async function fetchDataWinterFc() {
+    const res = await fetch(
+      `http://localhost:3000/${province}/winter/${year - 1}`,
+    );
+    res
+      .json()
+      .then(res => {
+        setWinterFc(res[0].date + ' ' + res[0].month);
+      })
+      .catch(err => setErrors(err));
+  }
 
   async function fetchDataSummer() {
     const res = await fetch(`http://localhost:3000/${province}/summer/${year}`);
     res
       .json()
       .then(res => {
-        setSummer(res[0].date + '/' + res[0].month);
+        setSummer(res[0].date + ' ' + res[0].month);
       })
       .catch(err => setErrors(err));
   }
@@ -52,7 +91,7 @@ const Predictor = props => {
     res
       .json()
       .then(res => {
-        setRainy(res[0].date + '/' + res[0].month);
+        setRainy(res[0].date + ' ' + res[0].month);
       })
       .catch(err => setErrors(err));
   }
@@ -62,30 +101,51 @@ const Predictor = props => {
     res
       .json()
       .then(res => {
-        setWinter(res[0].date + '/' + res[0].month);
+        setWinter(res[0].date + ' ' + res[0].month);
       })
       .catch(err => setErrors(err));
+  }
+
+  if (year == 2017) {
+    sunnyTMD = '3 March';
+    rainnyTMD = '16 May';
+    snowyTMD = '23 October';
+  } else if (year == 2018) {
+    sunnyTMD = '3 March';
+    rainnyTMD = '26 May';
+    snowyTMD = '27 October';
+  } else if (year == 2019) {
+    sunnyTMD = '21 February';
+    rainnyTMD = '20 May';
+    snowyTMD = '17 October';
+  } else if (year == 2020) {
+    sunnyTMD = '-- --';
+    rainnyTMD = '-- --';
+    snowyTMD = '-- --';
   }
 
   useEffect(() => {
     fetchDataSummer();
     fetchDataRainy();
     fetchDataWinter();
+    fetchDataSummerFc();
+    fetchDataRainyFc();
+    fetchDataWinterFc();
   }, [year, province]);
 
-  console.log(summer, rainy, winter);
+  console.log(summer, rainy, winter, summerFc, rainyFc, winterFc);
   return (
     <>
       <TextInner>Forcast</TextInner>
       <BoxInLine>
         <PredictorStyle>
-          <img src={SunnyImg} alt="Sunny Image" /> 15/03
+          <img src={SunnyImg} alt="Sunny Image" /> {summerFc}
         </PredictorStyle>
         <PredictorStyle>
-          <img src={RainyImg} alt="Rainny Image" /> 20/06
+          <img src={RainyImg} alt="Rainny Image" /> {rainyFc}
         </PredictorStyle>
         <PredictorStyle>
-          <img src={SnowyImg} alt="Snowy Image" /> 14/08
+          <img src={SnowyImg} alt="Snowy Image" /> {winterFc}
         </PredictorStyle>
       </BoxInLine>
       <TextInner>Actuality</TextInner>
